bf5801981c701faacfe1feeb33ce19ed51c17242
Edoardo Vanin <edoardo.vanin@gmail.com>
2020-02-11 21:41:58 +0100

    Modify the documentations and added a new property
    Added the property rain_loss for added the rain contribution to the link 
    budget calculation.

--- model/satellite.py
+++ model/satellite.py
@@ -1,8 +1,5 @@
-"""Module for modeling a satellite."""
+"""Module for modeling a satellite.
 
-import numpy as np
-
-"""
 The main aim of this module is model the satellite behaviour.
 This is not the agent of a RL problem.
 A better solution for modeling the environment described in a RL problem
@@ -12,6 +9,8 @@ The other (this file) modeling the satellite behaviour providing to the agent
 the solution space, the satellite state and other information for computing
 the optimization function.
 """
+
+import numpy as np
 
 
 class Satellite(object):
@@ -27,8 +26,8 @@ class Satellite(object):
         self._state.append(self._tot_pow)
 
     def next_state(self, actions):
-        """Calculate the next state action for the satellite."""
-        """
+        """Calculate the next state action for the satellite.
+
         Function that calculate the next state action for the satellite given
         the action taked in this step
         @param actions is a list containing all the actions for the satellite
@@ -54,17 +53,16 @@ class Satellite(object):
 
 
 class Beam(object):
-    """Model the satellite beam."""
+    """Model the satellite beam.
 
-    """
     This class will model the single satellite beam and all the possible
     attribute that it can manage like assignated power, number of user served
     by the beam, the beam loss till the earth and many others.
     """
 
     def __init__(self):
-        """Initialize the beam."""
-        """
+        """Initialize the beam.
+
         Constructor for initializing the beam with default values
         min_pow = 56, max_pow = 66 and EIRP = 61
         """
@@ -72,15 +70,20 @@ class Beam(object):
         self._EIRP = 61
         self._max_pow = 66
         self._min_pow = 56
-        self._EIRP_step = 0.1
+        self._EIRP_step = 0.5
 
         self._users = 0
         self._min_usr = 0
         self._max_urs = 10
         self._user_step = 1
 
+        self._rain_loss = 0.2  # Rain loss Initialize to 0
+
     def EIRP():
-        """EIRP property."""
+        """EIRP property.
+
+        Every time the EIRP is setted the CNR ratio is updated
+        """
         doc = "The EIRP property."
 
         def fget(self):
@@ -89,11 +92,28 @@ class Beam(object):
         def fset(self, value):
             if(self._min_pow <= value <= self._max_pow):
                 self._EIRP = value
+                self.update()
 
         def fdel(self):
             del self._EIRP
         return locals()
     EIRP = property(**EIRP())
+
+    def rain_loss():
+        """Modify the rain loss."""
+        doc = "The rain_loss property."
+
+        def fget(self):
+            return self._rain_loss
+
+        def fset(self, value):
+            self._rain_loss = value
+            self.update()
+
+        def fdel(self):
+            del self._rain_loss
+        return locals()
+    rain_loss = property(**rain_loss())
 
     def users():
         """User property."""
@@ -111,14 +131,25 @@ class Beam(object):
         return locals()
     users = property(**users())
 
+    def update(self):
+        r"""Update all the values that can be returned.
+
+        This function calculate the link budget over the beam using the formula
+        .. math:: \frac{C}{N_0} = P_{b,t} - \texit{OBO} + G_{T_x} + G_{R_x} +
+        \textit{FPSL} - L_{rain} - 10\log_{10}\left(kT_{sys}\right)
+        """
+        self._CNR = self._EIRP - self._OBO + self._gtx + self._grx + \
+            self._FPSL - self._rain - self._bolz
+
     def lighting(self, arg):
         """Set the lighting for the beam."""
         pass
 
     def get_actions(self):
-        """Return the possible action for the beam."""
-        """May be that we need to return always 3 actions"""
+        """Return the possible action for the beam.
 
+        May be that we need to return always 3 actions
+        """
         tmp_min = self._min_usr
         tmp_max = self._max_urs
         tmp = self._users
@@ -137,6 +168,13 @@ class Beam(object):
                                          (tmp_max - tmp + step) * 1 / step, 1)]
 
         return {'user': usr_actions, 'EIRP': EIRP_actions}
+
+    def next_step(self):
+        """Modify all the parameters for the next step.
+
+        This method will modify all the beam parameters that we want to change
+        from one step to the other.
+        """
 
 
 def random_state():
